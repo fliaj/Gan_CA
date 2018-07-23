c75ba56cdf5c1877a1b591d5e28ebf1aef6be39c
fliaj <lifuzhe0@qq.com>
2018-07-23 15:04:25 +0800

    dir fix

--- train.py
+++ train.py
@@ -88,9 +88,9 @@ if cuda:
     adversarial_loss.cuda()
 
 # Configure data loader
-os.makedirs('../../data/mnist', exist_ok=True)
+os.makedirs('./data/mnist', exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../../data/mnist', train=True, download=True,
+    datasets.MNIST('./data/mnist', train=True, download=True,
                    transform=transforms.Compose([
                        transforms.ToTensor(),
                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
